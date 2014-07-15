2ea755838f41ba5aa0010d6ca88f3422675a0127
Eric Courville <orgnaicit-git@outlook.com>
2014-07-14 23:26:15 -0700

    added actual html tags

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-<? echo "Hello, world!"; ?>
+<html>
+<head>
+  <title>Bay Azure UG Demo</title>
+</head>
+<body>
+<h1>Hello Bay Area Azure User Group</h1>
+<p><? echo "Hello, from Github Repo!"; ?></p>
+</body>
+</html>
+
+
